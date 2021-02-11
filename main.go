a2ef08f39aa5a167a2e2414f354df233a93a8b26
Smirnova Iuliia <44206037+SmirnovaIuliia@users.noreply.github.com>
2021-02-11 19:53:30 +0400

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	fmt.Println("Hello, Ѝउ")
-	fmt.Println("Hola!")
+func ShowFizzBuzz (numbers []int) {
+
+	for count, _ := range numbers {
+		
+		count += 1
+
+		if count%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if count%3 == 0 {
+			fmt.Println("Fizz")
+		} else if count%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(count)
+		}
+	}
+}
+
+func main () {	
+	
+	numbers := make([]int, 100)
+	
+	ShowFizzBuzz(numbers)
+	
 }
